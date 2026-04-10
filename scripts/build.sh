fc06a27a10d813898a6401abd7e61201e443006a
aidan <aidankeefe486@gmail.com>
2026-04-10 13:42:36 -0600

    fixed build script so it doens not need sudo run which breaks permissions for test script

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,15 +1,16 @@
 #!/usr/bin/env bash
 
+./scripts/test_fast.sh
+
 gcc -c -Iinclude -Wall -fPIC main.c -o main.o
 gcc -shared -o libAidan.so main.o
-mv libAidan.so /usr/lib/
-chmod 0755 /usr/lib/libAidan.so
-ldconfig
+sudo mv libAidan.so /usr/lib/
+sudo ldconfig
 
 libdir=/usr/include/LibAidan
 
-mkdir -p $libdir
+sudo mkdir -p $libdir
 
 for file in ./include/*.h; do
-  cp $file $libdir
+  sudo cp $file $libdir
 done
